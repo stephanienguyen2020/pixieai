d80cd1556a620077eb8520f132590c2b72a8f05f
sillysavant <long16012001.na@gmail.com>
2024-04-29 12:19:24 -0400

    fix bugs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,8 @@
-// Import the necessary modules
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
-import { NextApiRequest, NextApiResponse } from "next/types";
 
-// Define your authentication options
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -16,14 +13,6 @@ export const authOptions: NextAuthOptions = {
   ],
 };
 
-// Define a function to handle requests to this route
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    // Pass the request and response to the NextAuth handler along with the authentication options
-    await NextAuth(req, res, authOptions);
-  } catch (error) {
-    // Handle errors if any
-    console.error("Error during authentication:", error);
-    res.status(500).end("Error during authentication");
-  }
-};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
