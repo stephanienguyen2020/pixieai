f2595757f160963bcf2d177afcd991c8ef6b18d5
Stephanie Nguyen <tiennguyen.nltt@gmail.com>
2024-04-28 20:55:21 -0400

    Update

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+import prisma from "@/lib/prisma";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+    },
+    take: 1,
+  });
+
+  return [
+    {
+      url: "https://pixieai-frontend.vercel.app",
+      lastModified: new Date(),
+    },
+    ...users.map((user) => ({
+      url: `https://pixieai-frontend.vercel.app/${user.id}`,
+      lastModified: new Date(),
+    })),
+  ];
+}
